3e3e16efd35d991143529557c44a44f09f670be0
xiaoshuaima <theliuchenjun@icloud.com>
2018-11-12 16:20:21 +0800

    小帅马提交了文件

--- out/production/MFirstJavaWeb/.idea/dataSources/a9ac33ff-9263-4bb4-880f-346175e9d6b0/storage_v2/_staging_/schema/TEM_USER.Lna44A/view/ALL_MENUES.yzaWCg.sql
+++
@@ -1,8 +0,0 @@
-create or replace view ALL_MENUES as
-  (
-  select DISH.DISHID as dish_id, dish.name as dish_name,dish.price,dish.IMG,dish.DES,RESTAURANT.RESTAURANTID as rest_id ,restaurant.name as rest_name,restaurant.ADDRESS,RESTAURANT.PHONE,RESTAURANT.NOTIC,restaurant.FEE,restaurant.STARS
-  from menu join dish on menu.dishid=dish.dishid
-            join restaurant on  restaurant.restaurantid=menu.restaurantid
-  )
-
-
